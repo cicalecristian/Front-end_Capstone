542875508b2c0b35ab502f774a28deaa026adeb4
cicalecristian <cicalecristian@gmail.com>
2026-05-19 18:27:54 +0200

    creating LoginPage for testing

--- capstone/src/components/LoginPage.jsx
+++ capstone/src/components/LoginPage.jsx
@@ -1,24 +1,89 @@
-// import { useState } from "react"
+import { useState } from "react"
+import { useDispatch } from "react-redux"
+import { loginAction } from "../redux/actions/authAction.js"
+import { Button, Col, Container, Form, Row } from "react-bootstrap"
 
-// import { useDispatch } from "react-redux"
+const LoginPage = () => {
+  const dispatch = useDispatch()
 
-// import { loginAction } from "../redux/actions/authAction.js"
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  })
 
-// const LoginPage = () => {
-//   const dispatch = useDispatch()
+  const handleSubmit = (e) => {
+    e.preventDefault()
 
-//   const [formData, setFormData] = useState({
-//     email: "",
-//     password: "",
-//   })
+    dispatch(loginAction(formData))
+  }
 
-//   const handleSubmit = (e) => {
-//     e.preventDefault()
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <div className=" bg-dark d-flex flex-column align-items-center">
+            <h1 className=" text-white">LOGIN</h1>
+            <p className=" text-secondary">
+              please enter your login and password
+            </p>
+            <Form
+              onSubmit={handleSubmit}
+              className=" d-flex flex-column gap-3 w-75"
+            >
+              <div className=" d-flex flex-column">
+                <input
+                  className="form-control"
+                  style={{
+                    boxShadow: "none",
+                  }}
+                  type="email"
+                  placeholder="Email"
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({
+                      ...formData,
 
-//     dispatch(loginAction(formData))
-//   }
+                      email: e.target.value,
+                    })
+                  }
+                />
 
-//   return <></>
-// }
+                <br />
 
-// export default LoginPage
+                <input
+                  className="form-control"
+                  style={{ boxShadow: "none" }}
+                  type="password"
+                  placeholder="Password"
+                  value={formData.password}
+                  onChange={(e) =>
+                    setFormData({
+                      ...formData,
+
+                      password: e.target.value,
+                    })
+                  }
+                />
+              </div>
+              <a
+                href="#"
+                className=" text-secondary text-center text-decoration-none"
+              >
+                Forgot password?
+              </a>
+
+              <Button
+                type="submit"
+                className="btn-dark border-white border-2 hover-primary align-self-center px-4"
+              >
+                LOGIN
+              </Button>
+            </Form>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default LoginPage
